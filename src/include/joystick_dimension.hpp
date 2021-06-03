6304e5e16cb3481d269f71270feeaceb7830c259
Andy Little <kwikius@yahoo.com>
2021-06-03 15:48:58 +0100

    remove unneded joystick dimension dependency

--- src/include/joystick_dimension.hpp
+++ src/include/joystick_dimension.hpp
@@ -1,8 +1,9 @@
 #ifndef FG_EXT_JOYSTICK_CONTROL_SOURCE_HPP_INCLUDED
 #define FG_EXT_JOYSTICK_CONTROL_SOURCE_HPP_INCLUDED
 
-#include <flight_controller.hpp>
+#include <control_dimension.hpp>
 #include <quan/joystick.hpp>
+#include <quan/quantity_traits.hpp>
 
 template <FlightDimension D>
 struct joystick_dimension final : control_dimension<D>
