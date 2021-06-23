2f4010985233e4253f7d0076bdd7f446a0f57a70
Andy Little <kwikius@yahoo.com>
2021-06-23 16:34:13 +0100

    minor mods to io startup script

--- examples/io/bin/exec_flightgear.sh
+++ examples/io/bin/exec_flightgear.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 /usr/games/fgfs \
 --in-air \
---aircraft=ask13 \
+--aircraft=EasyStar \
 --prop:/input/joysticks/js[0]=0 \
 --units-meters \
 --altitude=800 \
---lat=50.7381 \
---lon=0.2494 \
+--lat=51.56856 \
+--lon=-4.2869 \
 --vc=10 \
 --glideslope=-3 \
 --native-fdm=socket,out,50,127.0.0.1,5600,udp \
 --telnet=socket,bi,50,localhost,5501,tcp
 
 
+ 
